a5ffd85c75fae4cb922fee452575bb9866038279
abouhmad <abouhmad@student.1337.ma>
2022-12-05 03:30:51 +0100

    cpp md3 ex01 v1

---
+++ cpp/module03/ex01/ScavTrap.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouhmad <abouhmad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/04 20:56:42 by abouhmad          #+#    #+#             */
+/*   Updated: 2022/12/04 20:58:03 by abouhmad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include <iostream>
+#include "ClapTrap.hpp"
+
+class ScavTrap
+{
+private:
+public:
+    ScavTrap();
+    ~ScavTrap();
+};
+
+#endif
