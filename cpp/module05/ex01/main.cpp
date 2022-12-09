2dc3b91dbac7af45324b47b6ebaee99739dc0fc0
abouhmad <abouhmad@student.1337.ma>
2022-12-09 21:26:09 +0100

    cpp md5 ex01 v1

--- cpp/module05/ex01/main.cpp
+++ cpp/module05/ex01/main.cpp
@@ -6,38 +6,24 @@
 /*   By: abouhmad <abouhmad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 01:12:32 by abouhmad          #+#    #+#             */
-/*   Updated: 2022/12/09 19:49:23 by abouhmad         ###   ########.fr       */
+/*   Updated: 2022/12/09 21:02:57 by abouhmad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+#include "Form.hpp"
 
 int main()
 {
-	Bureaucrat br("br", 150);
-	
-	br.incrementGrade();
-	std::cout << br;
-
 	try
 	{
-		Bureaucrat b("b", 0);
-		Form f("f", 2);
-		b.signForm(f);
-		std::cout << f;
+		Bureaucrat b("b", 2);
+		Form form("me", 3);
+		b.signForm(form);
+		std::cout << form;
 	}
 	catch (std::exception &e)
 	{
 		std::cout << e.what() << std::endl;
 	}
-	
-	try
-	{
-		Bureaucrat b("b", 151);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-	
 }
