e69fb8870872f093d97315eef036e3560a1ea3eb
ghostbhd <bhd.a@yandex.com>
2022-12-23 06:18:40 +0100

    md5 mod

---
+++ cpp/module05/ex02/PresidentialPardonForm.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.cpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouhmad <abouhmad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/23 06:11:42 by abouhmad          #+#    #+#             */
+/*   Updated: 2022/12/23 06:16:28 by abouhmad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PresidentialPardonForm.hpp"
+
+PresidentialPardonForm::PresidentialPardonForm() : Form("PresidentialPardonForm", 25, 5), _target("default")
+{
+}
+
+PresidentialPardonForm::PresidentialPardonForm(std::string target) : Form("PresidentialPardonForm", 25, 5), _target(target)
+{
+}
---
+++ cpp/module05/ex02/PresidentialPardonForm.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.hpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouhmad <abouhmad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/23 06:03:09 by abouhmad          #+#    #+#             */
+/*   Updated: 2022/12/23 06:15:15 by abouhmad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRESIDENTIALPARDONFORM_HPP
+# define PRESIDENTIALPARDONFORM_HPP
+
+# include "Form.hpp"
+
+class PresidentialPardonForm : public Form
+{
+private:
+    std::string _target;
+public:
+    PresidentialPardonForm();
+    PresidentialPardonForm(std::string target);
+    PresidentialPardonForm(const PresidentialPardonForm &copy);
+    PresidentialPardonForm &operator=(const PresidentialPardonForm &copy);
+    ~PresidentialPardonForm();
+
+    void execute(Bureaucrat const & executor) const;
+    
+};
+
+#endif
