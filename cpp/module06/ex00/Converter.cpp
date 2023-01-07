500f2cb096b87b881ac81a55b84a438b44279cc5
ghostbhd <bhd.a@yandex.com>
2023-01-08 00:37:38 +0100

    cpp md6 v2

---
+++ cpp/module06/ex00/Converter.cpp
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Converter.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouhmad <abouhmad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/08 00:07:22 by abouhmad          #+#    #+#             */
+/*   Updated: 2023/01/08 00:34:58 by abouhmad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Converter.hpp"
+
+Converter::Converter()
+{
+}
+
+Converter::Converter(Converter const &src)
+{
+	*this = src;
+}
+
+Converter::~Converter()
+{
+}
+
+Converter &Converter::operator=(Converter const &rhs)
+{
+	if (this != &rhs)
+	{
+		this->_int = rhs._int;
+		this->_float = rhs._float;
+		this->_double = rhs._double;
+		this->_char = rhs._char;
+		this->_isChar = rhs._isChar;
+		this->_isInt = rhs._isInt;
+		this->_isFloat = rhs._isFloat;
+		this->_isDouble = rhs._isDouble;
+	}
+	return (*this);
+}
+
+void Converter::convert(std::string str)
+{
+	this->_isChar = false;
+	this->_isInt = false;
+	this->_isFloat = false;
+	this->_isDouble = false;
+
+	if (isalpha(std::stoi(str)))
+	{
+		this->_isChar = true;
+		this->_char = static_cast<char>(std::stoi(str));
+	}
+	else
+	{
+		try
+		{
+			this->_int = std::stoi(str);
+			this->_isInt = true;
+		}
+		catch (std::exception &e)
+		{
+		}
+		try
+		{
+			this->_float = std::stof(str);
+			this->_isFloat = true;
+		}
+		catch (std::exception &e)
+		{
+		}
+		try
+		{
+			this->_double = std::stod(str);
+			this->_isDouble = true;
+		}
+		catch (std::exception &e)
+		{
+		}
+	}
+}
+
+void Converter::print()
+{
+	if (this->_isChar)
+		std::cout << "char: '" << this->_char << "'" << std::endl;
+	else
+		std::cout << "char: impossible" << std::endl;
+	if (this->_isInt)
+		std::cout << "int: " << this->_int << std::endl;
+	else
+		std::cout << "int: impossible" << std::endl;
+	if (this->_isFloat)
+		std::cout << "float: " << this->_float << "f" << std::endl;
+	else
+		std::cout << "float: nanf" << std::endl;
+	if (this->_isDouble)
+		std::cout << "double: " << this->_double << std::endl;
+	else
+		std::cout << "double: nan" << std::endl;
+}
--- cpp/module06/ex00/main.cpp
+++ cpp/module06/ex00/main.cpp
@@ -6,21 +6,22 @@
 /*   By: abouhmad <abouhmad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 12:56:26 by abouhmad          #+#    #+#             */
-/*   Updated: 2023/01/06 13:03:34 by abouhmad         ###   ########.fr       */
+/*   Updated: 2023/01/08 00:10:08 by abouhmad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "Converter.hpp"
 
 // convert a string to an integer, double, float, char, etc.
 int main(int ac, char **av)
 {
     if (ac != 2)
     {
-        std::cout << "Error: wrong number of arguments" << std::endl;
+        std::cout << "Usage: ./convert [string]" << std::endl;
         return (EXIT_FAILURE);
     }
-    std::string str = av[1];
-
+    Converter converter;
+    converter.convert(av[1]);
+    converter.print();
     return (EXIT_SUCCESS);
 }
