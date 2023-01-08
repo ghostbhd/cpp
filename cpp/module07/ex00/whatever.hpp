61922ed2fc42ae9c03f002cf2a468df3f5f4ee1f
ghostbhd <bhd.a@yandex.com>
2023-01-08 20:59:17 +0100

    cpp md7 v1

---
+++ cpp/module07/ex00/whatever.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouhmad <abouhmad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/08 20:56:47 by abouhmad          #+#    #+#             */
+/*   Updated: 2023/01/08 20:57:16 by abouhmad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b)
+{
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>
+T min(T &a, T &b)
+{
+    return (a < b ? a : b);
+}
+
+template <typename T>
+T max(T &a, T &b)
+{
+    return (a > b ? a : b);
+}
+
+#endif
