9df4bcf7272b5d59a44bb2d4460a7926d69c29d3
ghostbhd <bhd.a@yandex.com>
2023-01-12 22:52:29 +0100

    cpp md8 v1

--- cpp/module08/ex00/easyfind.hpp
+++ cpp/module08/ex00/easyfind.hpp
@@ -1,13 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abouhmad <abouhmad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/11 22:38:36 by abouhmad          #+#    #+#             */
-/*   Updated: 2023/01/11 22:38:41 by abouhmad         ###   ########.fr       */
+/*   Created: 2023/01/11 22:36:17 by abouhmad          #+#    #+#             */
+/*   Updated: 2023/01/11 22:38:15 by abouhmad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "easyfind.hpp"
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+
+template <typename T>
+int easyfind(T &container, int n)
+{
+    typename T::iterator it = std::find(container.begin(), container.end(), n);
+    if (it == container.end())
+        throw std::exception();
+    return *it;
+}
+
+#endif
