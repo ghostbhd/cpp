dca50926812d4b03789d6a57ad035356bcd943e4
ghostbhd <bhd.a@yandex.com>
2023-01-14 00:02:36 +0100

    cpp md8 v1.2

--- cpp/module08/ex01/main.cpp
+++ cpp/module08/ex01/main.cpp
@@ -6,13 +6,13 @@
 /*   By: abouhmad <abouhmad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 22:41:32 by abouhmad          #+#    #+#             */
-/*   Updated: 2023/01/13 22:42:40 by abouhmad         ###   ########.fr       */
+/*   Updated: 2023/01/14 00:01:37 by abouhmad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
-int main()
+/* int main()
 {
     try
     {
@@ -29,5 +29,18 @@ int main()
     {
         std::cout << e.what() << std::endl;
     }
+    return 0;
+} */
+
+int main()
+{
+    Span sp = Span(5);
+    sp.addNumber(6);
+    sp.addNumber(3);
+    sp.addNumber(17);
+    sp.addNumber(9);
+    sp.addNumber(11);
+    std::cout << sp.shortestSpan() << std::endl;
+    std::cout << sp.longestSpan() << std::endl;
     return 0;
 }
